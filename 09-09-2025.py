d4660bf323ca185f3e5660521028d855306c46a9
Alina <alina.meller.98@mail.ru>
2025-09-09 18:17:43 +0300

    Initial commit

---
+++ 09-09-2025.py
@@ -0,0 +1,40 @@
+# a = 'Hello'
+# b = 'Python'
+# print(a, b, a, b, sep='^')
+
+# print('Hello', end=' ')
+# print('Python')
+
+# a = 10
+# b = 3
+
+# print(a + b)
+# print(a - b)
+# print('a / b = ', a / b)
+# print('a // b = ', a // b)
+# print('a * b = ', a * b)
+# print('a % b = ', a % b)
+
+# candies = 20
+# students = 3
+# print(candies // students)
+
+# rub1 =int(input())
+# rub2 =int(input())
+# print(rub1 + rub2)
+
+num =int(input("Введите четырёхзначное число "))
+num1 = num // 1000
+num2 = num // 100 % 10
+num3 = num // 10 % 10
+num4 = num % 10
+print("Тысяч -", num1)
+print("Сотен -", num2)
+print("Десятков -", num3)
+print("Единиц -", num4)
+
+
+
+
+
+
