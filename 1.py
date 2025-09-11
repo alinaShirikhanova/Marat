de59de5ae382bcbf75a67f10f87a6b57bbbf7c69
Alina <alina.meller.98@mail.ru>
2025-09-11 23:26:45 +0300

    lesson2

--- 1.py
+++ 1.py
@@ -23,15 +23,15 @@
 # rub2 =int(input())
 # print(rub1 + rub2)
 
-num =int(input("Введите четырёхзначное число "))
-num1 = num // 1000
-num2 = num // 100 % 10
-num3 = num // 10 % 10
-num4 = num % 10
-print("Тысяч -", num1)
-print("Сотен -", num2)
-print("Десятков -", num3)
-print("Единиц -", num4)
+# num =int(input("Введите четырёхзначное число "))
+# num1 = num // 1000
+# num2 = num // 100 % 10
+# num3 = num // 10 % 10
+# num4 = num % 10
+# print("Тысяч -", num1)
+# print("Сотен -", num2)
+# print("Десятков -", num3)
+# print("Единиц -", num4)
 
 
 
