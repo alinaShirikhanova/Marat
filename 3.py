f53d0af825b9e81b9a220316025da2a7486788df
Alina <alina.meller.98@mail.ru>
2025-09-16 21:05:55 +0300

    lesson3

---
+++ 3.py
@@ -0,0 +1,121 @@
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# else if
+# if a % 2 == 0:
+#     print(a)
+# if b % 2 == 0:
+#     print(b)
+# if c % 2 == 0:
+#     print(c)
+
+# Пользователь вводит цвет:
+# красный
+# желтый
+# зеленый
+
+# a = input("Введите один из цветов сфетофора ")
+# if a == "красный":
+#     print("СТОП")
+# elif a == "желтый":
+#     print("Приготовься")
+# elif a == "зеленый":
+#     print("Иди")
+# else:
+#     print("некорректный цвет")
+
+
+
+
+# Пользователь вводит 3 числа. Распечатать только те,
+# которые являются положительными
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a >= 0:
+#     print(a)
+# if b >= 0:
+#     print(b)
+# if c >= 0:
+#     print(c)
+
+
+
+# Пользователь вводит оценку (2, 3, 4, 5)
+
+# a = int(input())
+# if a == 2:
+#     print("Это плохая оценка")
+# elif a == 3:
+#     print("Это удовлетворительная оценка")
+# elif a == 4:
+#     print("Это хорошая оценка")
+# elif a == 5:
+#     print("Это отличная оценка")
+# else:
+#     print("Такой оценки нету")
+
+# Алгоритм нахождения максимума или минимума
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+
+# maxi = a
+
+# if b > maxi:
+#     maxi = b
+# if c > maxi:
+#     maxi = c
+# if d > maxi:
+#     maxi = c
+# print(maxi)
+# if a >= b and a >= c and a >= d:
+#     print(a)
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# mini = a
+# if b < mini:
+#     mini = b
+# if c  < mini:
+#     mini = c
+# if d < mini:
+#     mini = d
+# print(mini)
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# number = 0
+#
+# if a % 2 == 0:
+#     number += a
+# if b % 2 == 0:
+#     number += b
+# if c % 2 == 0:
+#     number += c
+# if d % 2 == 0:
+#     number += d
+# print(number)
+
+age = int(input("Введите свой возраст: "))
+hasTicket = False
+
+if age >= 18:
+    if hasTicket == True:
+        print("Проходите")
+    else:
+        print("У вас нет билета")
+else:
+    print("Детям нельзя")
+
+
+
+
+
