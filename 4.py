9ee11b0080ad20c6ea8ca7f92ac70af0da261cd4
Alina <alina.meller.98@mail.ru>
2025-09-19 14:08:59 +0300

    lesson4

---
+++ 4.py
@@ -0,0 +1,113 @@
+# age = int(input("Введите свой возраст: "))
+# hasTicket = True
+#
+# if age >= 18:
+#     if hasTicket:
+#         print("Проходите")
+#     else:
+#         print("У вас нет билета")
+# else:
+#     print("Детям нельзя")
+
+
+
+
+# Пользователь вводит возраст и пол
+# - женщина
+# - девочка
+# - мужчина
+# - мальчик
+
+# a = int(input("Введите возраст "))
+# p = input("Введите пол ")
+# if a >= 18 and p == "женский":
+#     print("Женшина")
+# elif a < 18 and p == "женский":
+#     print("Девочка")
+# elif a >= 18 and p == "мужской":
+#     print("Мужчина")
+# elif a < 18 and p == "мужской":
+#     print("Мальчик")
+#
+# if p == "женский":
+#     if a >= 18:
+#         print("Женщина")
+#     else:
+#         print("Девочка")
+# else:
+#     if a >= 18:
+#         print("Мужчина")
+#     else:
+#         print("Мальчик")
+
+
+# rubles = 1000
+#
+# if rubles >= 1000:
+#     print("Можно")
+# else:
+#     print("Нельзя")
+
+
+# res = "Можно" if rubles >= 1000 else "Нельзя"
+# print(res)
+
+# Положительное
+# Отрицательное
+
+# num = int(input())
+# print("Положительное" if num >= 0 else "Отрицательное")
+# num1 = "Положительное" if num >= 0 else "Отрицательное"
+# print(num1)
+
+
+
+# line = 'Hello'
+# print(line[2])
+
+
+# n = "happy"
+# print(n[0])
+# print(n[1])
+# print(n[2])
+# print(n[3])
+# print(n[4])
+# print(n[5])
+
+# n = "happydfjfjlkdjsfkfjkjsfafjafksfsdfsdfsf"
+# print(n[-1])
+
+# a = "Jfhdsfkahjsssssssssfj"
+# print(a[-2])
+
+# length - длина
+
+# print(len('happy'))
+# a = 'happy'
+# print(len(a))
+
+
+
+# password = input()
+# if len(password) < 8:
+#     print("Ваш пароль слишком маленьким минимальная длинна 8 символов")
+# else:
+#     print("Ваш пароль принят")
+# оператор принадлежности
+# a = "password123"
+# print("1" in a)
+#
+# if "123" in a:
+#     print(" Цифра 1 есть в строке")
+# else:
+#     print(" Цифры 1 нет в строке")
+
+
+
+
+# pass
+a = "clpass"
+if "pass" in a:
+    print("В переменной a есть секретное слово")
+else:
+    print("В переменной a нету секротного слова")
