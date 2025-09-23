78be9ff849689ab518f464b382c8d52ca7f67fa7
Alina <alina.meller.98@mail.ru>
2025-09-23 18:56:01 +0300

    lesson5

---
+++ 5.py
@@ -0,0 +1,80 @@
+# line = "ситуация непростая"
+# Срезы
+# print(line[2:5])
+# print(line[0:8:2])
+# print(line[::3])
+# print(line[::-1])
+# print(line[::-1])
+
+# letters = 'a' * 100
+# print(letters)
+
+# Пользователь вводит строку
+# Распечатать строку умноженную на 10
+# a = input()
+# print(a * 10)
+
+
+# Пользователь вводит строку.
+# Необходимо создать новую строку, такую же, но в середину вставлена буква а
+# center = len(line) // 2
+
+# ПользоAватель
+# s = input()
+# word = len(s) // 2 # 2
+# print(s[:word] + "A" + s[word:])
+
+# Циклы
+
+# Цикл позволяет повторять какие-то действия сколько-то раз
+
+# for i in [100, 1, 34, 56]:
+#     print(i)
+
+
+
+
+# 5, 8, 1, 90
+# for i in (5, 8 , 1 ,90):
+#     print(i)
+
+# for letter in 'Marat':
+#     print(letter)
+
+
+
+
+
+# s = input()
+# for i in s:
+#     print(i)
+
+
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(501):
+#     print(i)
+
+
+# for i in range(10, 21):
+#     print(i)
+
+
+# a = int(input())
+# b = int(input())
+# for i in range(a,b + 1):
+#     print(i)
+
+
+# for i in range(1, 100, 2):
+#     print(i)
+
+
+# for i in range(0 , 101 , 5):
+#     print(i)
+
+
+# for i in range(10, 0, -1):
+#     print(i)
