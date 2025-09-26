cd319658caf1dda7f8b27dd6161e95560e181741
Alina <alina.meller.98@mail.ru>
2025-09-26 09:19:04 +0300

    lesson5

--- 5.py
+++ 5.py
@@ -5,6 +5,7 @@
 # print(line[::3])
 # print(line[::-1])
 # print(line[::-1])
+from ctypes.wintypes import tagPOINT
 
 # letters = 'a' * 100
 # print(letters)
@@ -32,8 +33,6 @@
 #     print(i)
 
 
-
-
 # 5, 8, 1, 90
 # for i in (5, 8 , 1 ,90):
 #     print(i)
@@ -42,13 +41,9 @@
 #     print(letter)
 
 
-
-
-
 # s = input()
 # for i in s:
 #     print(i)
-
 
 
 # for i in range(10):
@@ -78,3 +73,59 @@
 
 # for i in range(10, 0, -1):
 #     print(i)
+
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(10, 0, -1):
+#     print(i)
+# print("ЗАПУСК")
+
+
+# a = int(input())
+# b = int(input())
+# if a % 2 == 1:
+#     a += 1
+# for i in range(a, b + 1, 2):
+#     print(i)
+
+# a = int(input())
+# b = int(input())
+# for i in range(a, b + 1):
+#     if i % 2 == 0:
+#         print(i)
+
+# Распечатать числа от а до б, которые делятся на 3
+# a = int(input())
+# b = int(input())
+# for i in range(a,b + 1):
+#     if i % 3 == 0:
+#         print(i)
+
+#
+# a = int(input())
+# b = int(input())
+# if a % 3 == 1:
+#     a += 2
+# elif a % 3 == 2:
+#     a += 1
+# for i in range(a,b + 1, 3):
+#     print(i)
+
+
+# N = int(input())
+# b = 0
+# for i in range(10, 100):
+#     if i // 10 == N or i % 10 == N:
+#         b += i
+#print(b)
+# n = int(input())
+# for i in range(2, n + 1):
+#     if n % i == 0:
+#         print(i)
+#         break
+summ = 0
+for i in range(25,56):
+    summ += i * i * i
+print(summ)
