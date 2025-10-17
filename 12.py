ea50164245b56496a28099e01a1767cc8d0b8180
Alina <alina.meller.98@mail.ru>
2025-10-17 11:34:12 +0300

    lesson12

---
+++ 12.py
@@ -0,0 +1,49 @@
+# # Заполнение списка
+# n = int(input()) # кол-во элементов в списке n = 4
+# numbers = [] # пустой список
+#
+# for _ in range(n):
+#     numbers.append(int(input()))
+
+# for i in range(n):
+#     if numbers[i] % 2 == 0:
+#         print(numbers[i])
+
+
+
+# Заполнение списка
+# n = int(input()) # кол-во элементов в списке n = 4
+# numbers = [] # пустой список
+# f = 0
+# for _ in range(n):
+#     numbers.append(int(input()))
+#
+# for i in range(0, 5, 2):
+#     print(numbers[i])
+
+# 5
+# 130
+# 135
+# 137
+# 137
+# 138
+# 136
+
+
+# Заполнение списка
+n = int(input()) # кол-во элементов в списке n = 4
+numbers = [] # пустой список
+f = 0
+for _ in range(n):
+    numbers.append(int(input()))
+
+
+
+
+
+
+
+
+
+
+
